2ea5c857d188380b3d80050c6750e52d57fda007
AlexiaTonin <atoninelias@gmail.com>
2023-05-05 13:13:06 -0300

    landing route creada

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
 import Landing from './components/landingPage/Landing';
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
 
 function App() {
   return (
-    <Landing/>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Landing />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
