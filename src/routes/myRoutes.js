9608aa9fb4932084b9edac52370ba03a6b44b299
Angel <ancar1286@hotmail.com>
2023-05-07 09:53:22 -0300

    botones del landing redireccionan a destino

--- src/routes/myRoutes.js
+++ src/routes/myRoutes.js
@@ -2,6 +2,7 @@ import Landing from "../components/landingPage/Landing";
 import Home from "../components/home/Home";
 import Login from "../components/login/Login";
 import { Route, Routes } from "react-router-dom";
+import Register from "../components/register/Register";
 
 const MyRoutes = () => {
   return (
@@ -9,6 +10,7 @@ const MyRoutes = () => {
       <Route path="/" element={<Landing />} />
       <Route path="/home" element={<Home />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   );
 };
