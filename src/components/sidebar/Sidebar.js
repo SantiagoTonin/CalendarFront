13bc3c50167923c7b4758b85b45dcbc0f3b4f02c
unknown <atoninelias@gmail.com>
2023-05-14 21:24:10 -0300

    sidebar creado sin terminar

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,11 +1,45 @@
-import './sidebar.css'
+import React, { useState } from "react";
+import { FaBars } from "react-icons/fa";
+import {AiOutlineClose} from "react-icons/ai";
+import { Link } from "react-router-dom";
+import { SidebarData } from "./SidebarData.js";
+import "./sidebar.css";
+import { IconContext } from "react-icons";
 
 const Sidebar = () => {
-  return ( 
-    <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure aut accusamus est! Temporibus tempore ratione quae neque enim illo suscipit laborum in doloremque, unde veritatis itaque sapiente. Reiciendis, quam molestiae?
-    </div>
-   );
-}
- 
+  const [sidebar, setSidebar] = useState(false);
+  const showSidebar = () => setSidebar(!sidebar);
+
+  return (
+    <>
+      <IconContext.Provider value={{ color: "#fff" }}>
+        <div className="navbar">
+          <Link to="#" className="menu-bars">
+            <FaBars onClick={showSidebar} />
+          </Link>
+        </div>
+        <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+          <ul className="nav-menu-items" onClick={showSidebar}>
+            <li className="navbar-toggle">
+              <Link to="#" className="menu-bars">
+                <AiOutlineClose />
+              </Link>
+            </li>
+            {SidebarData.map((item, index) => {
+              return (
+                <li key={index} className={item.cName}>
+                  <Link to={item.path}>
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </IconContext.Provider>
+    </>
+  );
+};
+
 export default Sidebar;
