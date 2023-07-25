6ea1954d327981c157502ed60c8fc969fd560870
AlexiaTonin <atoninelias@gmail.com>
2023-07-25 17:29:21 -0300

    login creado falta agregar logica

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import MyRoutes from "./routes/myRoutes";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <MyRoutes />
+        <Footer/>
       </BrowserRouter>
     </>
   );
