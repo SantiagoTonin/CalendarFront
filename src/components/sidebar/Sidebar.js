582ea55db000b693d819ab3f347b5c76663a3b7b
AlexiaTonin <atoninelias@gmail.com>
2023-08-15 17:23:10 -0300

    logueado por roles y logout funcionando, salvo log como superAdmin

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,39 +1,71 @@
-import React, { useState } from "react";
+import React, { useState, useEffect} from "react";
 import { FaBars, FaEnvelopeOpenText, FaCartPlus } from "react-icons/fa";
 import { AiOutlineClose, AiFillHome } from "react-icons/ai";
 import { IoMdHelpCircle, IoIosPaper, IoMdPeople } from "react-icons/io";
 import { Link } from "react-router-dom";
-import { SidebarData } from "./SidebarData.js";
+import axiosInstance from "../../config/axiosInit.js";
 import "./sidebar.css";
 
 const Sidebar = () => {
   const [sidebar, setSidebar] = useState(false);
   const showSidebar = () => setSidebar(!sidebar);
+  const [token, setToken] = useState("");
+  const [userRol, setUserRol] = useState("");
+
+  useEffect(() => {
+    const token = sessionStorage.getItem("token");
+    if (token) {
+      getAuthStatus();
+      setToken(token);
+    } else setToken("");
+  }, []);
+
+  useEffect(() => {
+    if (token) {
+      
+    }
+  }, [userRol]);
+
+  const getAuthStatus = async () => {
+    try {
+      const { data } = await axiosInstance.get("/user?rol=");
+      setUserRol(data?.rol);
+    } catch (error) {
+      alert(error.response?.data?.message);
+    }
+  };
+
+  const handleLogOut = () => {
+    sessionStorage.clear();
+    window.location.replace("/");
+  };
 
   return (
     <>
       <section className="navbar">
-          <Link to="#" className="menuIcon">
-            <FaBars onClick={showSidebar} />
-          </Link>
-          <Link href="/home" className="navIcons">
-            <AiFillHome />
-          </Link>
-          <Link href="/" className="navIcons">
+        <Link to="#" className="menuIcon">
+          <FaBars onClick={showSidebar} />
+        </Link>
+        <Link href="/home" className="navIcons">
+          <AiFillHome />
+        </Link>
+        {userRol === "ADMINISTRADOR" ? (
+          <Link href="#" className="navIcons">
             <IoIosPaper />
           </Link>
-          <Link href="#" className="navIcons">
-            <FaCartPlus />
-          </Link>
-          <Link href="#" className="navIcons">
-            <IoMdPeople />
-          </Link>
-          <Link href="#" className="navIcons">
-            <FaEnvelopeOpenText />
-          </Link>
-          <Link href="#" className="navIcons">
-            <IoMdHelpCircle />
-          </Link>
+        ) : null}
+        <Link href="#" className="navIcons">
+          <FaEnvelopeOpenText />
+        </Link>
+        <Link href="#" className="navIcons">
+          <FaCartPlus />
+        </Link>
+        <Link href="#" className="navIcons" onClick={handleLogOut}>
+          <IoMdPeople />
+        </Link>
+        <Link href="#" className="navIcons">
+          <IoMdHelpCircle />
+        </Link>
       </section>
       <nav className={sidebar ? "navMenu active" : "navMenu"}>
         <ul className="navMenuItems" onClick={showSidebar}>
@@ -42,16 +74,30 @@ const Sidebar = () => {
               <AiOutlineClose />
             </Link>
           </li>
-          {SidebarData.map((item, index) => {
-            return (
-              <li key={index} className={item.cName}>
-                <Link to={item.path}>
-                  {item.icon}
-                  <span>{item.title}</span>
+          <>
+            <section className="navbar">
+              <Link to="/home" className="navText">
+                <AiFillHome /> Home
+              </Link>
+              {userRol === "ADMINISTRADOR" ? (
+                <Link to="#" className="navText">
+                  <IoIosPaper /> Usuarios
                 </Link>
-              </li>
-            );
-          })}
+              ) : null}
+              <Link to="#" className="navText">
+                <FaEnvelopeOpenText /> Mensajes
+              </Link>
+              <Link to="#" className="navText">
+                <FaCartPlus /> Perfil
+              </Link>
+              <Link to="#" className="navText" onClick={handleLogOut}>
+                <IoMdPeople /> Logout
+              </Link>
+              <Link to="#" className="navText">
+                <IoMdHelpCircle /> Ayuda
+              </Link>
+            </section>
+          </>
         </ul>
       </nav>
     </>
