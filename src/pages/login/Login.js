c2706df46c2917c64b42c9810043271cf26e89a9
AlexiaTonin <atoninelias@gmail.com>
2023-09-13 17:02:06 -0300

    cambios en modo light del login

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,11 +1,14 @@
+import { useContext } from "react";
+import { ThemeContext } from "../../context/ThemeContext";
 import LoginForm from "../../components/loginForm/LoginForm";
 import "../../components/loginForm/login.css";
 
 
 const Login = () => {
+  const { lightMode } = useContext(ThemeContext);
 
   return (
-    <main className="loginFormContainer">
+    <main className={lightMode ? "loginFormContainerLight" : "loginFormContainer"}>
       <h3>Login</h3>
       <LoginForm/>
     </main>
