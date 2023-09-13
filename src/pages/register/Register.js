8282e358b28bcf8c3b95f8e6067364791e380256
AlexiaTonin <atoninelias@gmail.com>
2023-09-13 17:26:24 -0300

    modo light register terminado

--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -1,9 +1,13 @@
+import { useContext } from "react";
+import { ThemeContext } from "../../context/ThemeContext";
 import "../../components/loginForm/login.css";
 import RegisterForm from "../../components/registerForm/RegisterForm";
 
 const Register = () => {
+  const { lightMode } = useContext(ThemeContext);
+
   return (
-    <main className="registerFormContainer">
+    <main className={lightMode ? "registerFormContainerLight" : "registerFormContainer"}>
       <h3>Registro</h3>
       <RegisterForm/>
     </main>
